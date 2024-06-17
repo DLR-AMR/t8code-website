25044c08105ff4db96d15d3c94ec58a69e512cd4
t8ddy <t8ddy.bot@gmail.com>
2024-06-17 08:38:20 +0000

    Add newest documentation

--- content/doc/dev/search/classes_1.js
+++ content/doc/dev/search/classes_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['ghost_5ffacejoins_5fstruct_1282',['ghost_facejoins_struct',['../structghost__facejoins__struct.html',1,'']]]
+  ['ghost_5ffacejoins_5fstruct_1284',['ghost_facejoins_struct',['../structghost__facejoins__struct.html',1,'']]]
 ];
