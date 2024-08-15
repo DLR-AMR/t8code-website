6038a7c2fdf232bad4b649fa81ec96e9dcdd7bcb
t8ddy <t8ddy.bot@gmail.com>
2024-08-15 14:33:10 +0000

    Add newest documentation

--- content/doc/dev/search/variables_5.js
+++ content/doc/dev/search/variables_5.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['face1_2285',['face1',['../structt8__stash__joinface.html#ae9c6c42fc8eddb85868a26bc376f555b',1,'t8_stash_joinface']]],
-  ['face2_2286',['face2',['../structt8__stash__joinface.html#a1bed248f8487b2df43a10bc2ec8faff8',1,'t8_stash_joinface']]],
-  ['face_5fknowledge_2287',['face_knowledge',['../structt8__cmesh.html#a30c51b123eba1694a8cf0d00c8194ddd',1,'t8_cmesh']]],
-  ['first_5fdesc_2288',['first_desc',['../structt8__tree.html#a60a6c1a3b010cbd24d6ead27a298a9b7',1,'t8_tree']]],
-  ['first_5flocal_5ftree_2289',['first_local_tree',['../structt8__forest.html#a0ff12e7168a5580243b7a8806adcbeef',1,'t8_forest']]],
-  ['first_5ftree_2290',['first_tree',['../structt8__cmesh.html#ae00cee31213b8116575b59e68b2fca30',1,'t8_cmesh']]],
-  ['first_5ftree_5fshared_2291',['first_tree_shared',['../structt8__cmesh.html#ae16f79bc83a4635714c3a71e5de7fb67',1,'t8_cmesh::first_tree_shared()'],['../structt8__cprofile.html#a0c20dbd615fc3734cbdebd439d8e7bf4',1,'t8_cprofile::first_tree_shared()']]],
-  ['from_5fmethod_2292',['from_method',['../structt8__forest.html#aa178dcfd6e93ee6856f3e961e4d092e1',1,'t8_forest']]]
+  ['face1_2287',['face1',['../structt8__stash__joinface.html#ae9c6c42fc8eddb85868a26bc376f555b',1,'t8_stash_joinface']]],
+  ['face2_2288',['face2',['../structt8__stash__joinface.html#a1bed248f8487b2df43a10bc2ec8faff8',1,'t8_stash_joinface']]],
+  ['face_5fknowledge_2289',['face_knowledge',['../structt8__cmesh.html#a30c51b123eba1694a8cf0d00c8194ddd',1,'t8_cmesh']]],
+  ['first_5fdesc_2290',['first_desc',['../structt8__tree.html#a60a6c1a3b010cbd24d6ead27a298a9b7',1,'t8_tree']]],
+  ['first_5flocal_5ftree_2291',['first_local_tree',['../structt8__forest.html#a0ff12e7168a5580243b7a8806adcbeef',1,'t8_forest']]],
+  ['first_5ftree_2292',['first_tree',['../structt8__cmesh.html#ae00cee31213b8116575b59e68b2fca30',1,'t8_cmesh']]],
+  ['first_5ftree_5fshared_2293',['first_tree_shared',['../structt8__cmesh.html#ae16f79bc83a4635714c3a71e5de7fb67',1,'t8_cmesh::first_tree_shared()'],['../structt8__cprofile.html#a0c20dbd615fc3734cbdebd439d8e7bf4',1,'t8_cprofile::first_tree_shared()']]],
+  ['from_5fmethod_2294',['from_method',['../structt8__forest.html#aa178dcfd6e93ee6856f3e961e4d092e1',1,'t8_forest']]]
 ];
